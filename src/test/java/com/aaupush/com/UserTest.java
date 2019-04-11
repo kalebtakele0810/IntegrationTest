573cfff75299a174cb7dc739dcd4b189b5ff4ef5
Kaleb Takele <kaleb.takele0810@gmail.com>
2019-04-11 09:49:22 +0300

    signup IT added

--- src/test/java/com/aaupush/com/UserTest.java
+++ src/test/java/com/aaupush/com/UserTest.java
@@ -34,15 +34,30 @@ public class UserTest {
 	}
 
 	@Test
-	public void test() {
+	public void addUser() {
 
 		Response response = RestAssured.given().contentType("application/json")
 				.body(new File("src/main/resources/User/addUser.json")).post("/UserServlet");
+
 		JsonObject jobj = new Gson().fromJson(response.asString(), JsonObject.class);
 		JsonElement status = jobj.get("status");
 		assertEquals(200, response.getStatusCode());
 		assertEquals("OK", status.getAsString());
 		assertTrue(this.getUser("3"));
+
+	}
+
+	@Test
+	public void signUp() {
+
+		Response response = RestAssured.given().contentType("application/json")
+				.body(new File("src/main/resources/User/signup.json")).post("/StudentServlet");
+
+		JsonObject jobj = new Gson().fromJson(response.asString(), JsonObject.class);
+		JsonElement status = jobj.get("status");
+		assertEquals(200, response.getStatusCode());
+		assertEquals("OK", status.getAsString());
+		assertTrue(this.getStudent("3"));
 
 	}
 
@@ -53,6 +68,29 @@ public class UserTest {
 			this.con = DriverManager.getConnection(this.databaseURL, this.userName, this.dbPassword);
 			Statement st = con.createStatement();
 			ResultSet rs = st.executeQuery("select * from aaupush.user where aaupush.user.Id='" + userID + "' ;");
+
+			while (rs.next()) {
+				if (!rs.getString("Id").isEmpty()) {
+					result = true;
+				}
+
+			}
+			rs.close();
+			con.close();
+		} catch (Exception e) {
+			fail("could not connect to the database!!");
+		}
+		return result;
+	}
+
+	public boolean getStudent(String studentID) {
+		boolean result = false;
+		try {
+			Class.forName(this.driver);
+			this.con = DriverManager.getConnection(this.databaseURL, this.userName, this.dbPassword);
+			Statement st = con.createStatement();
+			ResultSet rs = st
+					.executeQuery("select * from aaupush.student where aaupush.student.Id='" + studentID + "' ;");
 
 			while (rs.next()) {
 				if (!rs.getString("Id").isEmpty()) {
